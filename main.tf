484c22c583834e676cc0d3f7f4df7c7138559941
Meherun Mithila <mnm19322@email.vccs.edu>
2025-05-06 02:13:02 -0400

    All tf and yml codes are provided to successfully install vault

---
+++ main.tf
@@ -0,0 +1,30 @@
+variable "ami_id" {
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "zone_id" {
+  default = "Z040862479ELHY34R71D"
+}
+
+variable "tools" {
+  default = {
+    vault = {
+      instance_type = "t2.micro"
+      port          = 8200
+    }
+  }
+}
+
+module "tool-infra" {
+  source = "./module-infra"
+  for_each = var.tools
+  ami_id = var.ami_id
+  instance_type = each.value["instance_type"]
+  name = each.key
+  port = each.value["port"]
+  zone_id = var.zone_id
+}
